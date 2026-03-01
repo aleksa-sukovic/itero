0edc3808f8072fc4ccd25cea90daf9658d1e5123
Aleksa Šuković <sukovic.aleksa@gmail.com>
2026-03-01 08:15:24 +0100

    Fixes monitor brightness keybindings

--- migrations/20260301060800_add_brightness_key_env.sh
+++ migrations/20260301060800_add_brightness_key_env.sh
@@ -8,7 +8,7 @@ env_file="$ITERO_PATH/.env"
 
 if ! grep -q "BRIGHTNESS_KEY_UP" "$env_file"; then
     printf '\n# External monitor brightness shortcuts\n' >> "$env_file"
-    printf 'BRIGHTNESS_KEY_UP=<Super>MonBrightnessUp\n' >> "$env_file"
-    printf 'BRIGHTNESS_KEY_DOWN=<Super>MonBrightnessDown\n' >> "$env_file"
+    printf 'BRIGHTNESS_KEY_UP="<Super>MonBrightnessUp"\n' >> "$env_file"
+    printf 'BRIGHTNESS_KEY_DOWN="<Super>MonBrightnessDown"\n' >> "$env_file"
     log_ok "Added brightness key defaults to .env"
 fi
