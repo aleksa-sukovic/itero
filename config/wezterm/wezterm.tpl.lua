b3dcc6c38cfbb7e5425a224d0dd4a0cb512722e5
Aleksa Šuković <sukovic.aleksa@gmail.com>
2026-07-22 10:37:39 +0200

    Adds leader-based keybindings to wezterm

--- config/wezterm/wezterm.tpl.lua
+++ config/wezterm/wezterm.tpl.lua
@@ -69,11 +69,116 @@ config.colors = {
 }
 
 -- [[ Key bindings ]]
+config.leader = {
+    key = "s",
+    mods = "CTRL|ALT",
+    timeout_milliseconds = 1000,
+}
 config.keys = {
     {
-        key = "{",
-        mods = "CTRL|SHIFT",
+        key = "c",
+        mods = "LEADER",
+        action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+    },
+    {
+        key = "w",
+        mods = "LEADER",
+        action = wezterm.action.CloseCurrentTab({ confirm = false }),
+    },
+    {
+        key = "%",
+        mods = "LEADER|SHIFT",
+        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+    },
+    {
+        key = '"',
+        mods = "LEADER|SHIFT",
+        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+    },
+    {
+        key = "x",
+        mods = "LEADER",
+        action = wezterm.action.CloseCurrentPane({ confirm = false }),
+    },
+    {
+        key = "h",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection("Left"),
+    },
+    {
+        key = "j",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection("Down"),
+    },
+    {
+        key = "k",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection("Up"),
+    },
+    {
+        key = "l",
+        mods = "LEADER",
+        action = wezterm.action.ActivatePaneDirection("Right"),
+    },
+    {
+        key = "[",
+        mods = "LEADER",
         action = wezterm.action.ActivateCopyMode,
+    },
+    {
+        key = "q",
+        mods = "LEADER",
+        action = wezterm.action.QuickSelect,
+    },
+    {
+        key = "r",
+        mods = "LEADER",
+        action = wezterm.action.ReloadConfiguration,
+    },
+    {
+        key = "1",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(0),
+    },
+    {
+        key = "2",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(1),
+    },
+    {
+        key = "3",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(2),
+    },
+    {
+        key = "4",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(3),
+    },
+    {
+        key = "5",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(4),
+    },
+    {
+        key = "6",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(5),
+    },
+    {
+        key = "7",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(6),
+    },
+    {
+        key = "8",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(7),
+    },
+    {
+        key = "9",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(-1),
     },
 }
 
